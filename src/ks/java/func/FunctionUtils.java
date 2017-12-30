98e1a6e8648e45e3bc18300e24f4ea9415e579ad
k.d.szatkowski@gmail.com <k.d.szatkowski@gmail.com>
2017-12-30 16:11:38 +0100

    Simplified compose.

--- src/ks/java/func/FunctionUtils.java
+++ src/ks/java/func/FunctionUtils.java
@@ -3,12 +3,6 @@ package ks.java.func;
 public final class FunctionUtils {
 
 	public static Function<Integer, Integer> compose(Function<Integer, Integer> f, Function<Integer, Integer> g) {
-		return new Function<Integer, Integer>() {
-			
-			@Override
-			public Integer apply(Integer arg) {
-				return f.apply(g.apply(arg));
-			}
-		};
+		return x -> f.apply(g.apply(x));
 	}
 }
--- test/ks/java/func/FunctionUtilsTest.java
+++ test/ks/java/func/FunctionUtilsTest.java
@@ -1,7 +1,9 @@
 package ks.java.func;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class FunctionUtilsTest {
@@ -22,5 +24,20 @@ public class FunctionUtilsTest {
 		int result = squareTriple.apply(2);
 		
 		assertEquals(36, result);
+	}
+	
+	@Test
+	@Disabled
+	public void testComposeOverflow() {
+		final int composeitionsNum = 10000;
+		
+		Function<Integer, Integer> g = x -> x;
+		Function<Integer, Integer> f = x -> x + 1;
+		for (int i = 0; i < composeitionsNum; ++i) {
+			g = FunctionUtils.compose(f, g);
+		}
+		
+		// this call is supposed to cause stack overflow because of too many compositions
+		assertNotNull(g.apply(1));
 	}
 }
