5d10d6cf94494ed81126b3ccc866610b0044a64f
k.d.szatkowski@gmail.com <k.d.szatkowski@gmail.com>
2018-01-03 11:38:49 +0100

    Manual compsitions.

--- test/ks/java/func/FunctionUtilsTest.java
+++ test/ks/java/func/FunctionUtilsTest.java
@@ -40,4 +40,59 @@ public class FunctionUtilsTest {
 		// this call is supposed to cause stack overflow because of too many compositions
 		assertNotNull(g.apply(1));
 	}
+	
+	@Test
+	public void testDoubleManualCompose() {
+		Function<
+				// the first argument is a function x : int -> int ,e.g. square
+				Function<Integer, Integer>,
+				// the return type is a function taking a function y: int -> int and returning a function int -> int
+				Function<
+						Function<Integer, Integer>, 	// the second argument is a function int -> int, e.g. triple
+						Function<Integer, Integer>		// the return type is a function int -> int, e.g square the tripled value:)
+				>
+		> compose = x -> y -> z -> x.apply(y.apply(z));		// (int -> int) -> ((int -> int) -> (int -> int))
+															// same without unnecessary parentheses: (int -> int) -> (int -> int) -> int -> int
+		int result = compose.apply(new SquareFunction()).apply(new TripleFunction()).apply(2);
+		
+		assertEquals(36, result);
+	}
+	
+	@Test
+	public void testTripleManualCompose() {
+		// based on the example above it's quite easy to write triple composition
+		// (int -> int) -> ((int -> int) -> ((int -> int) -> (int -> int)))
+		// or without unnecessary parentheses: (int -> int) -> (int -> int) -> (int -> int) -> int -> int
+		Function<
+			Function<Integer, Integer>,
+			Function<
+				Function<Integer, Integer>,
+				Function<
+					Function<Integer, Integer>,
+					Function<Integer, Integer>
+				>
+			>
+		> compose = x -> y -> z -> arg -> x.apply(y.apply(z.apply(arg)));
+		
+		int result = compose.apply(new SquareFunction()).apply(new TripleFunction()).apply(new SquareFunction()).apply(2);
+		
+		assertEquals(144, result);
+	}
+	
+	@Test
+	public void testComposeSquareAndAdd() {
+		// when using type aliases like BinaryOperator types get a bit shorter;)
+		// (int -> int) -> (int -> int -> int) -> int -> int -> int
+		Function<
+			Function<Integer, Integer>,
+			Function<
+				BinaryOperator,
+				BinaryOperator
+			>
+		> compose = x -> y -> arg1 -> arg2 -> x.apply(y.apply(arg1).apply(arg2));
+		
+		int result = compose.apply(new SquareFunction()).apply(x -> y -> x + y).apply(2).apply(3);
+		
+		assertEquals(25, result);
+	}
 }
