664841411b4324bba66bb03e1ebf3f7e2ec48ca7
k.d.szatkowski@gmail.com <k.d.szatkowski@gmail.com>
2018-01-13 11:48:51 +0100

    Added solution for exercise 5.6. - init() with list reversing.

---
+++ explore/chapter5/ex5_6/solution/InitTest.java
@@ -0,0 +1,36 @@
+package chapter5.ex5_6.solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class InitTest {
+
+	@Test
+	public void testInitEmptyList() {
+		List<Integer> list = List.list();
+		
+		assertThrows(IllegalStateException.class, () -> list.init());
+	}
+	
+	@Test
+	public void testInitNonEmptyList() {
+		List<Integer> list = List.list(1,2,3);
+
+		List<Integer> newList = list.init();
+		
+		assertEquals("[1, 2, NIL]", newList.toString());
+	}
+	
+	@Test
+	public void testInitSingletonList() {
+		List<Integer> list = List.list(2);
+	
+		List<Integer> newList = list.init();
+
+		assertTrue(newList.isEmpty());
+	}
+	
+}
---
+++ explore/chapter5/ex5_6/solution/Supplier.java
@@ -0,0 +1,5 @@
+package chapter5.ex5_6.solution;
+
+public interface Supplier<T> {
+	T get();
+}
