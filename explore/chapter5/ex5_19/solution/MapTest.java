cfe333109a0b4cf31383c43d2d4085f952315efc
k.d.szatkowski@gmail.com <k.d.szatkowski@gmail.com>
2018-06-06 14:54:18 +0200

    Corrected names.

--- explore/chapter5/ex5_19/solution/MapTest.java
+++ explore/chapter5/ex5_19/solution/MapTest.java
@@ -8,21 +8,21 @@ import org.junit.jupiter.api.Test;
 public class MapTest {
 
 	@Test 
-	public void testTripleEmptyList() {
+	public void testMapEmptyList() {
 		List<Integer> list = List.list();
 		
 		assertThrows(IllegalStateException.class, () -> list.map(x -> 3 * x));
 	}
 	
 	@Test
-	public void testTripleNonEmptyList() {
+	public void testMapNonEmptyList() {
 		List<Integer> list = List.list(1,2,3);
 
 		assertEquals("[3, 6, 9, NIL]", list.map(x -> 3 * x).toString());
 	}
 	
 	@Test
-	public void testInitSingletonList() {
+	public void testMapSingletonList() {
 		List<Integer> list = List.list(2);
 
 		assertEquals("[6, NIL]", list.map(x -> 3 * x).toString());
